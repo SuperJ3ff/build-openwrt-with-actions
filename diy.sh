72ed4f771f30583210173064d96a2e34a1e598b7
SuperJ3ff <76992050+SuperJ3ff@users.noreply.github.com>
2026-08-14 07:10:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,6 +2,7 @@
 
 # 编辑默认的lan口ip地址
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
